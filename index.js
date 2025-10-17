cc91f5b5d19185ee89c77f22c551e24b56c3ce68
dhavalrw6 <rw6.dhaval.pl@gmail.com>
2025-10-17 13:59:03 +0530

    update

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import dotenv from "dotenv";
 import { db } from "./configs/db.js";
 import { User } from "./models/user.model.js";
 import bodyParser from "body-parser";
-import { LocalStorage } from "node-localstorage";
-import { userAuth } from "./middlewares/userAuth.middleware.js";
-global.localStorage = new LocalStorage('./scratch');
+import { Book } from "./models/book.model.js";
 
 dotenv.config();
 const port = process.env.PORT || 8081;
@@ -14,141 +12,89 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
 
-app.get("/",userAuth, (req, res) => {
+
+app.get("/", (req, res) => {
   return res.render("./index");
 });
 
 app.get("/form-basic", (req, res) => {
   return res.render("./pages/form-basic");
 });
-
-app.get("/tables", (req, res) => {
-  return res.render("./pages/tables");
+// create book
+app.post("/form-basic", async(req, res) => {
+  try {
+    await Book.create(req.body);
+    return res.redirect(req.get('Referrer') || '/');
+  } catch (error) {
+    console.log(error.message);    
+    return res.redirect(req.get('Referrer') || '/');
+  }
 });
+// View book
+app.get("/tables", async(req, res) => {
+    try {
+      let books = await Book.find({});
+      return res.render('./pages/tables',{
+        books
+      });
+    } catch (error) {
+      console.log(error.message);
+      return res.render('./pages/tables',{
+        books:[]
+      });
+    }
+});
+
+// Delete Book
+app.get('/book/delete/:id',async(req,res)=>{
+  try {
+    await Book.findByIdAndDelete(req.params.id);
+    console.log("Book Deleted.");
+    return res.redirect(req.get('Referrer') || '/');
+  } catch (error) {
+    console.log(error.message);    
+    return res.redirect(req.get('Referrer') || '/');
+  }
+});
+
+// render edit page
+app.get('/book/edit/:id',async(req,res)=>{
+  try {
+    let {id} = req.params;
+    let book = await Book.findById(id);
+    return res.render('./pages/editBook',{
+      book
+    });
+  } catch (error) {
+    console.log(error.message);    
+    return res.render('./pages/editBook',{
+      book:{}
+    });
+  }
+})
+
+app.post('/book/edit/:id',async(req,res)=>{
+  try {
+    let {id} = req.params;
+    await Book.findByIdAndUpdate(id,req.body);
+    return res.redirect('/tables');
+  } catch (error) {
+    console.log(error.message);    
+    return res.redirect(req.get('Referrer') || '/');
+  }
+})
+
 
 app.get("/login", (req, res) => {
   return res.render("./pages/login");
-});
-
-app.post("/login", async (req, res) => {
-  try {
-    let rs = await fetch("http://localhost:5674/user/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(req.body),
-    });
-
-    let data = await rs.json();
-    console.log("login route",data);    
-    if (data.success) {
-      console.log(data.data);
-      localStorage.setItem("id", JSON.stringify(data.data._id));
-      return res.redirect("/");
-    } else {
-      return res.redirect(req.get("Referrer") || "/");
-    }
-  } catch (error) {
-    console.log(error.message);
-    return res.redirect(req.get("Referrer") || "/");
-  }
-});
-// Login Api
-app.post("/user/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    let user = await User.findOne({ email });
-    if (user) {
-      if (user.password == password) {
-        return res.json({
-          message: "Login Success",
-          success: true,
-          data: user,
-        });
-      } else {
-        return res.json({ message: "Password Not match.", success: false });
-      }
-    } else {
-      return res.json({ message: "User Not Found.", success: false });
-    }
-  } catch (error) {
-    return res.json({ message: error.message, success: false });
-  }
 });
 
 app.get("/signup", (req, res) => {
   return res.render("./pages/signup");
 });
 
-app.post("/signup", (req, res) => {
-  console.log("data from signup", req.body);
 
-  fetch("http://localhost:5674/user/create", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(req.body),
-  })
-    .then((rs) => {
-      return rs.json();
-    })
-    .then((data) => {
-      console.log(data);
-      return res.redirect("/login");
-    })
-    .catch((error) => {
-      console.log(error);
-      return res.redirect(req.get("Referrer") || "/");
-    });
-});
-
-// singup Api
-app.post("/user/create", (req, res) => {
-  console.log("data from Api", req.body);
-  User.create(req.body)
-    .then((data) => {
-      return res.json(data);
-    })
-    .catch((error) => {
-      return res.json(error.message);
-    });
-});
-
-app.get("/user/getAllUser", (req, res) => {
-  User.find({})
-    .then((data) => {
-      return res.json(data);
-    })
-    .catch((error) => {
-      return res.json({ message: error.message });
-    });
-});
-
-app.get("/user/delete/:id", (req, res) => {
-  const { id } = req.params;
-  User.findByIdAndDelete(id)
-    .then((data) => {
-      return res.json({ message: "User Deleted.", data });
-    })
-    .catch((error) => {
-      return res.json({ message: error.message });
-    });
-});
-
-app.post("/user/update/:id", (req, res) => {
-  const { id } = req.params;
-  User.findByIdAndUpdate(id, req.body)
-    .then((data) => {
-      return res.json({ message: "User Updated.", id: data.id });
-    })
-    .catch((error) => {
-      return res.json({ message: error.message });
-    });
-});
 
 app.listen(port, (err) => {
   if (!err) {
